87c2e1f16b2345706681ab14a50aaa02275a4477
markus.gylling <markus.gylling@1702cad8-dd9a-11dd-9b7c-bbba4c9295f9>
2009-01-23 15:46:53 +0000

    Share project "org.daisy.emerson.ui.navigator" into "https://emerson-reader.googlecode.com/svn"

---
+++ trunk/org.daisy.emerson.ui.navigator/src/org/daisy/emerson/ui/navigator/ICommandIDs.java
@@ -0,0 +1,17 @@
+package org.daisy.emerson.ui.navigator;
+
+/**
+ * IDs of commands as defined in plugin.xml
+ * @author Markus Gylling
+ */
+public interface ICommandIDs {
+		
+	public static final String EXPAND_TREE = "org.daisy.emerson.commands.navigator.expandTreeView"; //$NON-NLS-1$
+	public static final String COLLAPSE_TREE = "org.daisy.emerson.commands.navigator.collapseTreeView"; //$NON-NLS-1$
+	public static final String NAVIGATE_HEADING = "org.daisy.emerson.commands.navigator.navigate.heading"; //$NON-NLS-1$
+	public static final String NAVIGATE_PAGE_PREVIOUS = "org.daisy.emerson.commands.navigator.navigate.page.previous"; //$NON-NLS-1$
+	public static final String NAVIGATE_PAGE_NEXT = "org.daisy.emerson.commands.navigator.navigate.page.next"; //$NON-NLS-1$
+	public static final String NAVIGATE_PAGE = "org.daisy.emerson.commands.navigator.navigate.page"; //$NON-NLS-1$
+	public static final String CATEGORY_PAGE_NAVIGATION = "org.daisy.emerson.ui.navigator.category.pages"; //$NON-NLS-1$
+	
+}
---
+++ trunk/org.daisy.emerson.ui.navigator/src/org/daisy/emerson/ui/navigator/handlers/NavigatePrevPhraseHandler.java
@@ -0,0 +1,21 @@
+package org.daisy.emerson.ui.navigator.handlers;
+
+import org.daisy.reader.model.navigation.Direction;
+import org.daisy.reader.model.semantic.Semantic;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+/**
+ * Navigate to adjacent position of a given semantic.
+ * @author Markus Gylling
+ */
+public class NavigatePrevPhraseHandler extends NavigateAdjacentHandler {
+
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		super.direction = Direction.PREV;
+		super.semantic = Semantic.PHRASE;
+		return super.execute(event);
+	}
+	
+
+}
