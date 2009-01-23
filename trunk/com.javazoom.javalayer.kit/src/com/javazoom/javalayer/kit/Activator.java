56eddcde68025776b6c0658392d1ee382f5a738e
markus.gylling <markus.gylling@1702cad8-dd9a-11dd-9b7c-bbba4c9295f9>
2009-01-23 15:02:09 +0000

    Share project "com.javazoom.javalayer.kit" into "https://emerson-reader.googlecode.com/svn"

---
+++ trunk/com.javazoom.javalayer.kit/src/com/javazoom/javalayer/kit/Activator.java
@@ -0,0 +1,35 @@
+package com.javazoom.javalayer.kit;
+
+import org.eclipse.core.runtime.Plugin;
+import org.eclipse.core.runtime.Status;
+import org.osgi.framework.BundleContext;
+
+public class Activator extends Plugin {
+
+	public static final String PLUGIN_ID = "com.javazoom.javalayer.kit"; //$NON-NLS-1$
+
+	private static Activator plugin;
+	
+	public Activator() {
+		
+	}
+
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	public static Activator getDefault() {
+		return plugin;
+	}
+
+	public void logError(String message, Throwable exception) {
+		getLog().log(new Status(Status.ERROR,PLUGIN_ID,message,exception));	
+	}
+	
+}
