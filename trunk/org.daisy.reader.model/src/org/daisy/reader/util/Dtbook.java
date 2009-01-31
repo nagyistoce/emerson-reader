3d1fb34b55c8ac2162b8c7378f9b92fc0e465ef9
markus.gylling <markus.gylling@1702cad8-dd9a-11dd-9b7c-bbba4c9295f9>
2009-01-31 15:26:50 +0000

    Add http-equiv for IE, fix base URI value

--- trunk/org.daisy.reader.model/src/org/daisy/reader/util/Dtbook.java
+++ trunk/org.daisy.reader.model/src/org/daisy/reader/util/Dtbook.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URISyntaxException;
 import java.net.URL;
 
 import javax.xml.stream.XMLInputFactory;
@@ -22,24 +23,26 @@ public class Dtbook {
 	 * <li>Change of default namespace</li>
 	 * <li>Change of root element name</li>
 	 * <li>adding xml:base and head/base@href</li>
+	 * <li>adding http-equiv</li>
 	 * </ul>
 	 * @param source The source DTBook File
 	 * @param dest The file to stream to
 	 * @param modifyRelativeLinks Whether to modify relative links to point to the source location
 	 * @return The created File
-	 * @throws Exception 
-	 */
-		
-	public static File htmlize(URL source, File dest, boolean modifyRelativeLinks) throws IOException, XMLStreamException {
+	 * @throws URISyntaxException 
+	 * @throws XMLStreamException
+	 * @throws IOException
+	 */		
+	public static File htmlize(URL source, File dest, boolean modifyRelativeLinks) throws IOException, XMLStreamException, URISyntaxException {
 		XMLInputFactory xif = null;			
 		XMLOutputFactory xof = null;
 		InputStream is = null;
 		XMLStreamReader reader = null;
 		XMLStreamWriter writer = null;
-					
+						
 		try{
 						
-			String base = URIStringParser.getBase(source.getPath());
+			String base = URIStringParser.getBase(source.toURI().toASCIIString());
 			String cssURI = null;
 			
 			xif = StAXFactoryProxy.getXMLInputFactory();
@@ -49,7 +52,6 @@ public class Dtbook {
 			writer = xof.createXMLStreamWriter(new FileOutputStream(dest),UTF_8); 
 			
 			boolean firstStartElement = true;
-			//String defaultNamespace = "";
 			String defaultNamespace = XHTML_NS; 
 			writer.writeStartDocument(UTF_8, "1.0"); //$NON-NLS-1$
 			while(reader.hasNext()) {				
@@ -120,8 +122,11 @@ public class Dtbook {
 									writer.writeAttribute(TYPE, TEXT_CSS);
 							}
 							
-						}
-						
+							//<meta http-equiv="Content-Type" content="application/xhtml+xml; charset=utf-8"/>
+							writer.writeStartElement(META);
+							writer.writeAttribute(HTTP_EQUIV, CONTENT_TYPE);
+							writer.writeAttribute(CONTENT, "application/xhtml+xml; charset=utf-8"); //$NON-NLS-1$							
+						}						
 						break;
 					case XMLStreamConstants.END_ELEMENT:
 						writer.writeEndElement();	
@@ -150,7 +155,6 @@ public class Dtbook {
 						writer.writeProcessingInstruction(reader.getPITarget(), data);
 						break;
 					default:	
-						//System.err.println("skipping event type " + eventType);
 						break;
 				}
 			}		
@@ -162,7 +166,7 @@ public class Dtbook {
 		return dest;
 	}
 	
-	public static File htmlize(URL source, File dest) throws IOException, XMLStreamException {
+	public static File htmlize(URL source, File dest) throws IOException, XMLStreamException, URISyntaxException {
 		return Dtbook.htmlize(source, dest, false);
 	}
 	
@@ -188,7 +192,11 @@ public class Dtbook {
 	private static final String HEAD = "head"; //$NON-NLS-1$
 	private static final String SRC = "src"; //$NON-NLS-1$
 	private static final String IMG = "img"; //$NON-NLS-1$
+	private static final String META = "meta"; //$NON-NLS-1$
 	private static final String XML_BASE = "xml:base"; //$NON-NLS-1$
 	private static final String XHTML_NS = "http://www.w3.org/1999/xhtml"; //$NON-NLS-1$
 	private static final String UTF_8 = "utf-8"; //$NON-NLS-1$
+	private static final String HTTP_EQUIV = "http-equiv"; //$NON-NLS-1$
+	private static final String CONTENT_TYPE ="Content-Type"; //$NON-NLS-1$
+	private static final String CONTENT ="content"; //$NON-NLS-1$
 }
