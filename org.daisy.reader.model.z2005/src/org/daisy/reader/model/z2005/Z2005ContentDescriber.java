7bc7af194f1c31ff965711b892e6ffc277e92ff4
markus.gylling@gmail.com <markus.gylling@gmail.com@1702cad8-dd9a-11dd-9b7c-bbba4c9295f9>
2010-03-06 09:59:06 +0000

    update to 3.5 contentType extension point (core.contenttype.contentTypes instead of deprecated core.runtime.contentTypes)

--- org.daisy.reader.model.z2005/src/org/daisy/reader/model/z2005/Z2005ContentDescriber.java
+++ org.daisy.reader.model.z2005/src/org/daisy/reader/model/z2005/Z2005ContentDescriber.java
@@ -9,13 +9,8 @@ import org.eclipse.core.runtime.content.IContentDescription;
 
 public class Z2005ContentDescriber implements IContentDescriber {
 
-	public Z2005ContentDescriber() {
-		
-	}
-
 	public int describe(InputStream contents, IContentDescription description)
 			throws IOException {
-		//TODO ContentDescriber, changed extension point in 3.5
 		return IContentDescriber.VALID;
 	}
 
