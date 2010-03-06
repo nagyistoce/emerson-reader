436e29d10090f2d06ffc07cfa28368e7fde722e4
markus.gylling@gmail.com <markus.gylling@gmail.com@1702cad8-dd9a-11dd-9b7c-bbba4c9295f9>
2010-03-06 10:21:20 +0000

    Clean up.

--- trunk/org.daisy.emerson.ui/src/org/daisy/emerson/ui/handlers/ShowKeyBindingDialogHandler.java
+++ trunk/org.daisy.emerson.ui/src/org/daisy/emerson/ui/handlers/ShowKeyBindingDialogHandler.java
@@ -7,30 +7,10 @@ import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.keys.IBindingService;
 
 public class ShowKeyBindingDialogHandler extends AbstractHandler {
-//	private static final String COMMAND_PREFIX = "org.daisy.emerson";
 	
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-    	IBindingService bindingService;
-    	bindingService = (IBindingService) PlatformUI.getWorkbench().getService(IBindingService.class);
-    	
-//    	ICommandService commandService;
-//    	commandService = (ICommandService) PlatformUI.getWorkbench().getService(ICommandService.class);
-//    	List<String> commands = new ArrayList<String>(); 
-//    	commands.addAll(commandService.getDefinedCommandIds());
-//    	Collections.sort(commands);
-//    	Iterator<?> iterator = commands.iterator();
-//    	int i = 0;
-//    	while(iterator.hasNext()) {
-//    		String cID = (String) iterator.next();
-//    		
-//    		if(cID.startsWith(COMMAND_PREFIX)) {
-//    			i++;
-//    			System.err.print("Command: " + cID + " :: ");
-//    			System.err.println("Key: " + bindingService.getBestActiveBindingFormattedFor(cID));
-//    		}
-//    	}
-//    	System.err.println("counter was " +i);   	
-    	
+    	IBindingService bindingService = 
+    		(IBindingService) PlatformUI.getWorkbench().getService(IBindingService.class);    	
     	bindingService.openKeyAssistDialog();    	    	
 		return null;
 	}
